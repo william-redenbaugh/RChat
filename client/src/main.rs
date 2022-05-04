91727b57cf5bcbeded2a66c62839f50645cd3d9d
William Redenbaugh <wredenba@amazon.com>
2022-05-03 22:58:55 -0700

    staring RPC communication

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,18 +1,32 @@
-use std::{alloc::handle_alloc_error};
-
+use std::net::SocketAddr;
+use tarpc::{client, context, tokio_serde::formats::Json};
 pub struct MessengerConnection{
     ip: String, 
     port: String, 
-    
+    server_addr: SocketAddr,
 }
 
-fn new_connection(ip: String, port: String, ){
+fn new_connection(ip_in: String, port_in: String)->MessengerConnection{
+    
+    let mut str = ip_in.clone();
+    str.push(':'); 
+    str.push_str(&port_in.as_str()); 
 
+    let mut serv_addr = str.parse().expect("Could not parse"); 
+
+    let mut transport = tarpc::serde_transport::tcp::connect(serv_addr, Json::default);
+
+    return MessengerConnection { 
+        ip: ip_in,
+        port: port_in ,
+        server_addr: serv_addr
+    };
 }
 
 impl MessengerConnection{
-    pub fn get_messages(&mut self){
+    pub fn get_messages(&mut self) -> String{
 
+        return String::from("");
     }
 
     pub fn send_message(&mut self, msg: String){
@@ -20,14 +34,22 @@ impl MessengerConnection{
     }
 }
 
+#[tokio::main]
 fn main() {
-    let is_writing = handle_input(); 
 
+    let mut conn = new_connection(String::from("192.168.1.75"), String::from("1212"));
+    let is_writing = handle_input(); 
+    
     if is_writing {
-        
+        let mut input = String::new(); 
+        let input_type = std::io::stdin().read_line(&mut input).unwrap();
+        println!("What is your message?: ");
+        conn.send_message(input);
     }
     else{
 
+        let messages = conn.get_messages();
+        println!("Message Output: {}", messages);
     }
 }
 
